e9c41136025c61d3e0ce322d06d5964ae28cd9a2
Hyunsu Kim <hyunsu.kim@snu.ac.kr>
2024-11-25 20:34:39 +0900

    sec

---
+++ src/Test/Test1.cpp
@@ -0,0 +1,26 @@
+#include "include/Test/Test1.hpp"
+
+#include <chrono>
+#include <iostream>
+#include <random>
+#include <string>
+#include <thread>
+
+// #include "API/HXRHapticsAPI.hpp"
+
+// API Test
+namespace Test {
+
+void Test1() {
+  std::cout << "=============Test1=============" << std::endl;
+
+
+
+
+  
+
+
+  std::cout << "=============End=============" << std::endl;
+}
+}; // namespace Test
+
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "include/Test/Test1.hpp"
+// #include "Test2.hpp"
+// #include "Test3.hpp"
+
+
+int main(){
+    Test::Test1();
+    return 0;
+}
